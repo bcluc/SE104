3f6abdf29eda9c245ec290dc406862e786e1313e
Dinh Duy <hodnhduy10@gmail.com>
2023-06-21 19:32:32 +0700

    chinh sua phan lam hoi chieu

--- app/src/main/java/com/example/se104/choose_trip.java
+++ app/src/main/java/com/example/se104/choose_trip.java
@@ -6,11 +6,21 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class choose_trip extends AppCompatActivity {
 
     Button nextButton; //Khai báo một button
     Button backButton; //Khai báo một button
+
+
+
+    private RecyclerView flight_ticket;
+    private TicketAdapter ticketAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +40,38 @@ public class choose_trip extends AppCompatActivity {
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(choose_trip.this, flight_info.class);
+                Intent intent = new Intent(choose_trip.this, check_out.class);
                 startActivity(intent);
             }
         });
+
+
+        flight_ticket = findViewById(R.id.flight_list);
+        ticketAdapter = new TicketAdapter(this);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        flight_ticket.setLayoutManager(linearLayoutManager);
+
+        ticketAdapter.setData(getListTicket());
+
+        flight_ticket.setAdapter(ticketAdapter);
+    }
+
+
+
+
+    private List<Ticket> getListTicket() {
+        List<Ticket> ticketList = new ArrayList<>();
+        ticketList.add(new Ticket("HAN", "09:00", "7h00p", "ICN", "15:00", "5.000.000", "Còn 3 chỗ"));
+        ticketList.add(new Ticket("SGN", "07:00", "2h30p", "HAN", "09:30", "1.500.000", "Còn 10 chỗ"));
+        ticketList.add(new Ticket("HAN", "09:00", "7h00p", "ICN", "15:00", "5.000.000", "Còn 3 chỗ"));
+        ticketList.add(new Ticket("SGN", "07:00", "2h30p", "HAN", "09:30", "1.500.000", "Còn 10 chỗ"));
+        ticketList.add(new Ticket("HAN", "09:00", "7h00p", "ICN", "15:00", "5.000.000", "Còn 3 chỗ"));
+        ticketList.add(new Ticket("SGN", "07:00", "2h30p", "HAN", "09:30", "1.500.000", "Còn 10 chỗ"));
+        ticketList.add(new Ticket("HAN", "09:00", "7h00p", "ICN", "15:00", "5.000.000", "Còn 3 chỗ"));
+        ticketList.add(new Ticket("SGN", "07:00", "2h30p", "HAN", "09:30", "1.500.000", "Còn 10 chỗ"));
+        return ticketList;
+
     }
 
 }
-
