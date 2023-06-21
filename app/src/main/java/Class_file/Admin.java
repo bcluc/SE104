85bcdaba4a59dbfe2d600a71bbe00127b45d02b0
ThuHuong03 <ntthuong010603@gmail.com>
2023-06-21 15:43:12 +0700

    new admin

--- app/src/main/java/Class_file/Admin.java
+++ app/src/main/java/Class_file/Admin.java
@@ -5,7 +5,16 @@ public class Admin {
     private String Password;
     private String Name;
     private String Phone;
+    private String Email;
     private String Airline_Brand;
 
 
+    public Admin(String id, String password, String name, String phone,String email, String airline_brand) {
+        ID = id;
+        Password = password;
+        Name = name;
+        Phone = phone;
+        Email= email;
+        Airline_Brand = airline_brand;
+    }
 }
--- app/src/main/java/com/example/se104/acitivity_new_admin2.java
+++ app/src/main/java/com/example/se104/acitivity_new_admin2.java
@@ -7,15 +7,27 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class acitivity_new_admin2 extends AppCompatActivity {
+
     EditText user_name;
     EditText password;
+    EditText name;
+    EditText Phone;
+    EditText Email;
+    EditText Airline_Brand;
     Button btn_back;
     Button btn_next;
+    Button btn_back_2;
+    Button btn_done;
+    LinearLayout admin1;
+    LinearLayout admin2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,8 +36,22 @@ public class acitivity_new_admin2 extends AppCompatActivity {
 
         user_name= findViewById(R.id.text_ID);
         password= findViewById(R.id.text_pass);
+        name= findViewById(R.id.text_name);
+        Phone= findViewById(R.id.text_phone);
+        Email= findViewById(R.id.text_email);
+        Airline_Brand= findViewById(R.id.text_Plane_ID);
+
         btn_back = findViewById(R.id.button_back);
         btn_next= findViewById(R.id.button_next);
+
+        admin1= findViewById(R.id.layout_admin2);
+        admin2= findViewById(R.id.layout_admin3);
+
+        btn_back_2= findViewById(R.id.button_back_2);
+        btn_done= findViewById(R.id.bttn_done);
+
+        admin1.setVisibility(View.VISIBLE);
+        admin2.setVisibility(View.GONE);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,17 +107,112 @@ public class acitivity_new_admin2 extends AppCompatActivity {
         });
 
         btn_next.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
+                if(user_name.length()==0 || password.length()==0 )
+                {
+                    Toast.makeText(getApplicationContext(), "Mời bạn nhập đủ thông tin", Toast.LENGTH_LONG).show();
+                }else
+                {
+                    admin1.setVisibility(View.GONE);
+                    admin2.setVisibility(View.VISIBLE);
+                }
 
-                    Intent intent = new Intent(activity_new_admin2.this, acitivity_new_admin3.class);
+            }
+        });
 
-                    startActivity(intent);
+        btn_back_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                admin1.setVisibility(View.VISIBLE);
+                admin2.setVisibility(View.GONE);
+            }
+        });
 
 
+        name.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        }});
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length() ==0)
+                {
+                    name.setError("Bạn phải nhập Họ và tên ");
+
+                }
+                else
+                    name.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        Phone.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length() ==0)
+                {
+                    Phone.setError("Bạn phải nhập Số điện thoại ");
+
+                }
+                else
+                    Phone.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        Email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.length() ==0)
+                {
+                    Email.setError("Bạn phải nhập Email");
+
+                }
+                else
+                    Email.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        btn_done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(name.length()==0 || Phone.length()==0 || Email.length() ==0 )
+                {
+                    Toast.makeText(getApplicationContext(), "Mời bạn nhập đủ thông tin", Toast.LENGTH_LONG).show();
+                }
+
+                else {
+
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thành công!", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
 
 
