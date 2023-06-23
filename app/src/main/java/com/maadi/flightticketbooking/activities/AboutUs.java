65abaf5846a9ef08b1de8ad64737ed4f568ea466
bcluc <21521898@gm.uit.edu.vn>
2023-06-24 05:25:41 +0700

    update

--- app/src/main/java/com/maadi/flightticketbooking/activities/AboutUs.java
+++ app/src/main/java/com/maadi/flightticketbooking/activities/AboutUs.java
@@ -1,17 +1,57 @@
 package com.maadi.flightticketbooking.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.maadi.flightticketbooking.R;
+import com.maadi.flightticketbooking.models.Developer;
 
 
 public class AboutUs extends AppCompatActivity {
 
+    private Developer origin = new Developer();
+    private Button btn_video;
+    private Button btn_pay;
+    private Button btn_call;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        btn_video = (Button) findViewById(R.id.btn_video_demo);
+        btn_pay = (Button) findViewById(R.id.btn_video_pay);
+        btn_call = (Button) findViewById(R.id.btn_call);
+
+        btn_video.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = origin.getUrl_demo();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn_pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = origin.getUrl_pay();
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        });
+        btn_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse(origin.getPhone_number()));
+                startActivity(callIntent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/maadi/flightticketbooking/models/Developer.java
@@ -0,0 +1,34 @@
+package com.maadi.flightticketbooking.models;
+
+public class Developer {
+    private String url_demo = "http://www.google.com";
+    private String url_pay = "http://www.google.com";
+    private String phone_number = "tel:" + "01234567";
+
+    public Developer() {
+    }
+
+    public String getUrl_demo() {
+        return url_demo;
+    }
+
+    public void setUrl_demo(String url_demo) {
+        this.url_demo = url_demo;
+    }
+
+    public String getUrl_pay() {
+        return url_pay;
+    }
+
+    public void setUrl_pay(String url_pay) {
+        this.url_pay = url_pay;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+}
